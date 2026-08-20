d60b7a2fc3b6376af090fbc1f9a5ec2e5948839d
shridharb11 <shridharb1106@gmail.com>
2026-08-20 23:52:58 +0530

    defined DIFT

--- rtl/core/ibex_core/dv/uvm/core_ibex/tb/core_ibex_tb_top.sv
+++ rtl/core/ibex_core/dv/uvm/core_ibex/tb/core_ibex_tb_top.sv
@@ -1,7 +1,7 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-
+`define DIFT
 `include "uvm_macros.svh"
 `include "dv_macros.svh"
 `include "prim_assert.sv"

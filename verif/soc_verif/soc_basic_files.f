f5adbb9f5d75da521ad97f511e5edae095059fa7
hyper-mania14 <golashmaanya@gmail.com>
2026-08-22 01:43:24 +0530

    modified soc_basic_files.f

--- verif/soc_verif/soc_basic_files.f
+++ verif/soc_verif/soc_basic_files.f
@@ -4,20 +4,18 @@
 // =============================================================================
 
 // Base filelist that correctly includes the Prim libraries, DIFT, and Ibex Core (WITHOUT UVM)
--f ../files.f
+// (Must be executed from the project root)
+-f verif/files.f
 
 // OBI and APB Includes
-+incdir+../../rtl/obi_wrapper
-+incdir+../../rtl/peripheral/apb_uart
++incdir+rtl/obi_wrapper
++incdir+rtl/peripheral/apb_uart
 
 // OBI to APB Bridge
-../../rtl/obi_wrapper/obi_to_apb.sv
-
-// APB UART Submodule
-// (Already compiled by ../files.f, no need to include here)
+rtl/obi_wrapper/obi_to_apb.sv
 
 // Basic SoC Top
-../../rtl/soc/basic_soc_top.sv
+rtl/soc/basic_soc_top.sv
 
 // Exhaustive Testbench
-basic_soc_tb.sv
+verif/soc_verif/basic_soc_tb.sv

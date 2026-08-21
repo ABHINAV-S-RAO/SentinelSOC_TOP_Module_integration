a604d3efbd8aaf8a08c7f614f7ff8c2c02eed248
VibhuKavBhat <vibhukav@gmail.com>
2026-08-21 19:21:44 +0530

    Updated qspi_files.f

--- verif/files_qspi_block.f
+++ verif/files_qspi_block.f
@@ -1,7 +1,12 @@
-// QSPI RTL Files
+// QSPI RTL Submodules
 rtl/peripheral/apb_spi_master/spi_master_apb_if.sv
 rtl/peripheral/apb_spi_master/spi_master_fifo.sv
+rtl/peripheral/apb_spi_master/spi_master_clkgen.sv
+rtl/peripheral/apb_spi_master/spi_master_tx.sv
+rtl/peripheral/apb_spi_master/spi_master_rx.sv
 rtl/peripheral/apb_spi_master/spi_master_controller.sv
+
+// QSPI Top Level
 rtl/peripheral/apb_spi_master/apb_spi_master.sv
 
 // Testbench

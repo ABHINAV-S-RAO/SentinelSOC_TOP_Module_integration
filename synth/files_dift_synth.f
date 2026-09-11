f4ede90aa12e493f6fde5232a73476fc629a8fe3
ABHINAV-S-RAO <abhinavsraohp@gmail.com>
2026-09-11 21:14:38 +0530

    changed synth.tcl

--- synth/files_dift_synth.f
+++ synth/files_dift_synth.f
@@ -15,5 +15,7 @@ rtl/core/dift/ibex_dift_mem.sv
 rtl/core/dift/ibex_dift_tmu.sv
 rtl/core/dift/ibex_register_file_latch_tag.sv
 
+rtl/core/ibex_core/vendor/lowrisc_ip/ip/prim_generic/rtl/prim_ram_1p_pkg.sv
+
 -f rtl/core/ibex_core/rtl/ibex_core.f
 rtl/core/ibex_core/rtl/ibex_top.sv

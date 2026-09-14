24678c70615d7820b5d81a907a6e510660ff3f00
ABHINAV-S-RAO <abhinavsraohp@gmail.com>
2026-09-14 11:19:02 +0530

    changed crypto path in files.f

--- verif/debugger/files.f
+++ verif/debugger/files.f
@@ -57,6 +57,21 @@ rtl/Interrupts/plic/plic_top.sv
 # verif/plic_verif/plic_tb_pkg.sv
 # verif/plic_verif/tb_plic_top.sv
 
++rtl/crypto/SHA/sha512_pkg.sv
++rtl/crypto/ALU/pseudo_mersenne.sv
++rtl/crypto/ALU/multiplier.sv
++rtl/crypto/ALU/alu.sv
++rtl/crypto/ALU/alu_top.sv
++rtl/crypto/SHA/sha512_msg_sched.sv
++rtl/crypto/SHA/sha512_padder.sv
++rtl/crypto/SHA/sha512_round.sv
++rtl/crypto/SHA/sha512_top.sv
++rtl/crypto/ed/reg_file.sv
++rtl/crypto/ed/micro_seq.sv
++rtl/crypto/ed/master_fsm.sv
++rtl/crypto/ed/top_ed25519.sv
++rtl/crypto/otp.sv
++rtl/crypto/top_most.sv
 
 
 rtl/crypto/top_most.sv
